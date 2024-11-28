e3cb52c986c745504bd4b430b55ba85ea3a0c121
TANACHOT-CHAISIN <s6704062616125@email.kmutnb.ac.th>
2024-11-28 18:30:22 +0700

    V0.5
    - ⛔Remove News Format input

--- lab1minx.c
+++ lab1minx.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){
 	int a,b;
-	printf("Enter First Number And Another Number: ");
-	scanf("%d %d",&a,&b);
+	printf("Enter First Number: ");
+	scanf("%d",&a);
+	printf("Enter Another: ");
+	scanf("%d",&b);
 	if (a > b){
-		printf("%d more than %d  diff: %d",a,b,(a-b));
+		printf("%d more than %d  |diff: %d",a,b,(a-b));
 	}
 	else{
-		printf("%d more than %d diff: %d",b,a,(b-a));
+		printf("%d more than %d |diff: %d",b,a,(b-a));
 	}
 	return 0;
 }
